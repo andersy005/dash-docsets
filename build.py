18b61011ce5afc848f8af3178c92d9defde135dc
Anderson Banihirwe <axbanihirwe@ualr.edu>
2020-01-19 16:32:07 -0700

    Add Feeds as well

--- build.py
+++ build.py
@@ -12,6 +12,8 @@ HOME_DIR = Path(".").absolute()
 DOCSET_DIR = HOME_DIR / "docsets"
 ICON_DIR = HOME_DIR / "icons"
 DOCSET_DIR.mkdir(parents=True, exist_ok=True)
+FEED_DIR = HOME_DIR / "feeds"
+FEED_DIR.mkdir(parents=True, exist_ok=True)
 
 
 @contextlib.contextmanager
@@ -27,11 +29,39 @@ def working_directory(path):
         os.chdir(prev_cwd)
 
 
+def create_feed(project_info):
+    from xml.etree.ElementTree import Element, SubElement, tostring
+    from bs4 import BeautifulSoup
+    from github import Github
+
+    g = Github()
+
+    repo = g.get_repo(project_info["repo"])
+    latest_tag = list(repo.get_tags())[0].name
+
+    feed_filename = f"{FEED_DIR}/{project_info['name']}.xml"
+    base_url = (
+        "https://raw.githubusercontent.com/andersy005/dash-docsets/docsets/docsets"
+    )
+
+    entry = Element("entry")
+    version = SubElement(entry, "version")
+    version.text = f"master.post.{latest_tag}"
+    url = SubElement(entry, "url")
+    url.text = f"{base_url}/{project_info['name']}.tgz"
+
+    bs = BeautifulSoup(tostring(entry), features="html.parser").prettify()
+
+    with open(feed_filename, "w") as f:
+        f.write(bs)
+
+
 def build_docset(project_info, local_store):
     """
     Build Dash Docset for a project
     """
-    repo_link = project_info["repo"]
+    base_url = "https://github.com"
+    repo_link = f"{base_url}/{project_info['repo']}.git"
     folder_name = local_store / project_info["name"]
     doc_dir = folder_name / project_info["doc_dir"]
     cmd = ["git", "clone", repo_link, folder_name.as_posix()]
@@ -65,6 +95,24 @@ def build_docset(project_info, local_store):
         cmd += icons
 
     subprocess.check_call(cmd)
+
+    with working_directory(DOCSET_DIR):
+
+        tar_cmd = [
+            "tar",
+            "--exclude='.DS_Store'",
+            "-cvzf",
+            f"{project_info['name']}.tgz",
+            f"{project_info['name']}.docset",
+        ]
+
+        subprocess.check_call(tar_cmd)
+
+        cmd = ["rm", "-rf", f"{project_info['name']}.docset"]
+
+        subprocess.check_call(cmd)
+
+    create_feed(project_info)
 
 
 def _main():
