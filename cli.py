93828adc6d0f83ca600dc90ad99b1d0fbaf34070
Anderson Banihirwe <axbanihirwe@ualr.edu>
2021-03-20 21:14:12 -0600

    Stream output

--- cli.py
+++ cli.py
@@ -3,7 +3,9 @@ import functools
 import itertools
 import operator
 import os
+import re
 import subprocess
+import sys
 import tempfile
 import traceback
 from pathlib import Path
@@ -35,6 +37,43 @@ FEED_DIR = HOME_DIR / "feeds"
 FEED_DIR.mkdir(parents=True, exist_ok=True)
 
 
+def _stream_command(cmd, no_newline_regexp="Progess", **kwargs):
+    """stream a command (yield) back to the user, as each line is available.
+    # Example usage:
+    results = []
+    for line in stream_command(cmd):
+        print(line, end="")
+        results.append(line)
+    Parameters
+    ==========
+    cmd: the command to send, should be a list for subprocess
+    no_newline_regexp: the regular expression to determine skipping a
+                       newline. Defaults to finding Progress
+    """
+
+    if isinstance(cmd, str):
+        cmd = cmd.split(" ")
+
+    console.log(cmd)
+
+    process = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, **kwargs
+    )
+    for line in iter(process.stdout.readline, ""):
+        if not re.search(no_newline_regexp, line):
+            yield line
+    process.stdout.close()
+    return_code = process.wait()
+    if return_code:
+        print(process.stderr.read(), file=sys.stderr)
+        raise subprocess.CalledProcessError(return_code, cmd)
+
+
+def stream_command(cmd, no_newline_regexp="Progess", **kwargs):
+    for _ in _stream_command(cmd, no_newline_regexp, **kwargs):
+        pass
+
+
 def validate_generator(generator: str):
     generators = ["doc2dash", "html2dash", "dash-webgen"]
     if generator not in generators:
@@ -59,7 +98,7 @@ def _build_project(
     repo,
     doc_dir='docs',
     html_pages_dir='_build/html',
-    doc_build_cmd="make -j4 html",
+    doc_build_cmd="make -j10 html",
     generator="doc2dash",
     install=True,
     url=None,
@@ -68,7 +107,7 @@ def _build_project(
     local_dir = REPODIR / name
     doc_dir = local_dir / doc_dir
     validate_generator(generator)
-    kwargs = dict(shell=True, check=True)
+    kwargs = dict()
 
     if not local_dir.exists():
         repo_link = f"{BASE_URL}/{repo}"
@@ -79,7 +118,7 @@ def _build_project(
             repo_link,
             local_dir,
         ]
-        subprocess.run(command, check=True)
+        stream_command(command)
     else:
         console.log(f"{name} directory already exits.")
 
@@ -87,14 +126,14 @@ def _build_project(
 
         if install and generator != "dash-webgen":
             command = ["python", "-m", "pip", "install", ".", "--no-deps"]
-            subprocess.run(command, check=True)
+            stream_command(command)
         latest_tag = os.popen("git rev-parse --short HEAD").read().strip()
         if not latest_tag:
             latest_tag = "unknown"
     if generator != "dash-webgen":
         with working_directory(doc_dir):
             command = doc_build_cmd
-            subprocess.run(command, **kwargs)
+            stream_command(command, **kwargs)
 
     icon_dir = ICON_DIR / name
     icons = []
@@ -121,7 +160,7 @@ def _build_project(
             icons = list(itertools.chain(*icons))
             command += icons
         command = " ".join(command)
-        subprocess.run(command, **kwargs)
+        stream_command(command, **kwargs)
 
     elif generator == "html2dash":
         if icon_files:
@@ -162,8 +201,8 @@ def _build_project(
         # Compress the result docset with maximum compression with xz:
         my_env = os.environ.copy()
         my_env["XZ_OPT"] = "-9"
-        subprocess.run(tar_command, check=True, env=my_env)
-        subprocess.run(f"rm -rf {dir_to_delete}", **kwargs)
+        stream_command(tar_command, env=my_env)
+        stream_command(f"rm -rf {dir_to_delete}", **kwargs)
     create_feed(name, latest_tag)
 
 
@@ -197,7 +236,7 @@ def build(
         '_build/html', help='location of built html pages relative to `doc_dir`'
     ),
     doc_build_cmd: str = typer.Option(
-        "make -j4 html", help='custom command to use when building the docs. Defaults to None'
+        "make -j10 html", help='custom command to use when building the docs. Defaults to None'
     ),
     generator: str = typer.Option("doc2dash", help="Documentation Set generator."),
     install: bool = typer.Option(True, help="Whether to install the package in editable mode"),
